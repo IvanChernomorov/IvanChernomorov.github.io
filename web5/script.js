112908bed7f14301bd9e7e2eac9822b36ed8d40f
Ivan Chernomorov <90576997+IvanChernomorov@users.noreply.github.com>
2021-10-20 01:32:22 +0300

    Add files via upload

---
+++ web5/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function calc() {
+    let price = document.getElementsByName("price");
+    let quan = document.getElementsByName("quantity");
+    if(/^[1-9][0-9]*$/.test(price[0].value) && /^\d+$/.test(quan[0].value))
+        alert("Итоговая цена = " + parseInt(price[0].value) * parseInt(quan[0].value));
+    else
+        alert("Введите корректные данные!");
+}
+window.addEventListener("DOMContentLoaded",
+    function (event) 
+    {
+        let b = document.getElementById("result");
+        b.addEventListener("click", calc);
+    }
+);
+
