d57600d77d92df496da6397ac1f4e9ae8264054b
Ivan Chernomorov <90576997+IvanChernomorov@users.noreply.github.com>
2021-10-22 12:08:14 +0300

    Update script.js

--- web5/script.js
+++ web5/script.js
@@ -3,13 +3,14 @@
 function calc() {
     let price = document.getElementsByName("price");
     let quan = document.getElementsByName("quantity");
-    if(/^[1-9][0-9]*$/.test(price[0].value) && /^[1-9][0-9]*$/.test(quan[0].value))
-        alert("Итоговая цена = " + parseInt(price[0].value) * parseInt(quan[0].value));
+    if((/^[1-9][0-9]*$/).test(price[0].value) && (/^[1-9][0-9]*$/).test(quan[0].value))
+    { alert("Итоговая цена = " + parseInt(price[0].value) *
+           parseInt(quan[0].value));}
     else
-        alert("Введите корректные данные!");
+    { alert("Введите корректные данные!");}
 }
 window.addEventListener("DOMContentLoaded",
-    function (event) 
+    function (event)
     {
         let b = document.getElementById("result");
         b.addEventListener("click", calc);
